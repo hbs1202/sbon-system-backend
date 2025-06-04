68a0ff2b33bbe1764a3ee5bd22c3047f8a99968e
hbs1202 <102892620+hbs1202@users.noreply.github.com>
2025-06-04 23:10:56 +0900

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
-import authRoutes from './routes/auth';
+import studentRoutes from './routes/auth';
 import outingRoutes from './routes/outingRoutes';
 import stayRoutes from './routes/stayRoutes';
 
@@ -15,7 +15,7 @@ console.log('서버 초기화 시작...');
 
 // CORS 설정
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://web-sbon-system-frontend-mbhiy4va1af0e6e0.sel4.cloudtype.app/'],
+  origin: ['https://web-sbon-system-frontend-mbhiy4va1af0e6e0.sel4.cloudtype.app', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
@@ -28,12 +28,12 @@ app.use((req, res, next) => {
 
 // 라우트 설정
 console.log('라우터 등록 시작...');
-console.log('auth 라우터 등록...');
-app.use('/api', authRoutes);
 console.log('outing 라우터 등록...');
 app.use('/api/outing', outingRoutes);
 console.log('stay 라우터 등록...');
 app.use('/api/stay', stayRoutes);
+console.log('student 라우터 등록...');
+app.use('/api/student', studentRoutes);
 console.log('라우터 등록 완료');
 
 // 기본 라우트
